1814fcfc05204b01ebb44985d04f854a23a0d16e
Franciole Bezerra <franciole.bezerra@gmail.com>
2018-11-05 17:52:05 -0200

    Primeiro Commit

---
+++ FinanceiroWeb/src/br/com/financeiro/util/RNException.java
@@ -0,0 +1,26 @@
+package br.com.financeiro.util;
+
+public class RNException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public RNException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public RNException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RNException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RNException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
